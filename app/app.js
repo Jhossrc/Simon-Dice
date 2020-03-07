9aab7dac855c796ea9c07fa6a03b35630a9844e2
Jhoss <kuro.rc10@gmail.com>
2020-03-07 10:05:01 -0500

    Creando botones del juego

---
+++ app/app.js
@@ -0,0 +1,7 @@
+// BOTONES DEL JUEGO
+
+const skyblue = document.getElementById('skyblue')
+const violet = document.getElementById('violet')
+const orange = document.getElementById('orange')
+const green = document.getElementById('green')
+const btnStart = document.getElementById('btnStart')
