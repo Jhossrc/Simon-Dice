119e12635a471262b94b8087031c0f52aa70e1cd
Jhoss <kuro.rc10@gmail.com>
2020-03-07 12:13:56 -0500

    Obteniendo el input del usuario, se aplico el bind y eventos click para los botones de colores

--- app/app.js
+++ app/app.js
@@ -10,40 +10,44 @@ const btnStart = document.getElementById('btnStart')
 
 class Game {
     constructor() {
-        this.initialize()
-        this.generateSequence()
-        this.nextLevel()
+        this.chooseColor = this.chooseColor.bind(this)
+        this.initialize();
+        this.generateSequence();
+        this.nextLevel();
     }
 
     initialize() {
-        btnStart.classList.add('hide');
-        this.level = 1
+        btnStart.classList.add("hide");
+        this.level = 1;
         this.colors = {
             skyblue,
             violet,
             orange,
             green
-        }
+        };
     }
 
     generateSequence() {
-        this.sequence = new Array(10).fill(0).map(number => Math.floor(Math.random() * 4))
+        this.sequence = new Array(10)
+            .fill(0)
+            .map(number => Math.floor(Math.random() * 4));
     }
 
     nextLevel() {
-        this.illuminateSequence()
+        this.illuminateSequence();
+        this.addEventClick();
     }
 
     transformNumberToColor(number) {
         switch (number) {
             case 0:
-                return 'skyblue'
+                return "skyblue";
             case 1:
-                return 'violet'
+                return "violet";
             case 2:
-                return 'orange'
+                return "orange";
             case 3:
-                return 'green'
+                return "green";
         }
     }
 
@@ -52,20 +56,32 @@ class Game {
             const color = this.transformNumberToColor(this.sequence[i]);
 
             setTimeout(() => {
-                this.illuminateColor(color)
+                this.illuminateColor(color);
             }, 1000 * i);
         }
     }
 
-    turnOffColor(color) {
-        this.colors[color].classList.remove('light')
+    illuminateColor(color) {
+        this.colors[color].classList.add("light");
+        setTimeout(() => {
+            this.turnOffColor(color);
+        }, 350);
     }
 
-    illuminateColor(color) {
-        this.colors[color].classList.add('light')
-        setTimeout(() => {
-            this.turnOffColor(color)
-        }, 350);
+    turnOffColor(color) {
+        this.colors[color].classList.remove("light");
+    }
+
+    addEventClick() {
+        this.colors.skyblue.addEventListener('click', this.chooseColor)
+        this.colors.violet.addEventListener('click', this.chooseColor)
+        this.colors.orange.addEventListener('click', this.chooseColor)
+        this.colors.green.addEventListener('click', this.chooseColor)
+    }
+
+    chooseColor(e) {
+        console.log(e);
+        console.log(this);
     }
 }
 
