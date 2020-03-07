6d4a13d3f12295d2d3b8758c37ab35f71c7637bf
Jhoss <kuro.rc10@gmail.com>
2020-03-07 10:07:51 -0500

    Creando clase Game

--- app/app.js
+++ app/app.js
@@ -5,3 +5,15 @@ const violet = document.getElementById('violet')
 const orange = document.getElementById('orange')
 const green = document.getElementById('green')
 const btnStart = document.getElementById('btnStart')
+
+// CLASE JUEGO
+
+class Game {
+    constructor() {
+        this.initialize()
+    }
+
+    initialize() {
+        btnStart.classList.add('hide');
+    }
+}
